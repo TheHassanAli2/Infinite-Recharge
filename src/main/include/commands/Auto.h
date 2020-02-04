692244d7d8d81475cf0821e1dce91d7287fd387b
Daniel Agapov <dlern1@ocdsb.ca>
2020-02-04 15:52:11 -0500

    Create path import framework for PathWeaver

---
+++ src/main/include/commands/Auto.h
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc2/command/CommandBase.h>
+#include <frc2/command/CommandHelper.h>
+#include <frc/Filesystem.h>
+#include <frc/trajectory/TrajectoryUtil.h>
+#include <wpi/Path.h>
+//#include <wpi/SmallVectorImpl.h>
+#include <wpi/SmallString.h>
+
+/**
+ * An example command.
+ *
+ * <p>Note that this extends CommandHelper, rather extending CommandBase
+ * directly; this is crucially important, or else the decorator functions in
+ * Command will *not* work!
+ */
+class Auto
+    : public frc2::CommandHelper<frc2::CommandBase, Auto> {
+ public:
+  Auto();
+
+  void Initialize() override;
+
+  void Execute() override;
+
+  void End(bool interrupted) override;
+
+  bool IsFinished() override;
+
+private:
+  wpi::SmallString<64> deployDirectory;
+  frc::Trajectory trajectory;
+};
