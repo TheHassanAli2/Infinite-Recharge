baffe06c950da1102c5de43454401ce41fc203fc
Daniel Agapov <dlern1@ocdsb.ca>
2020-02-10 15:24:49 -0500

    Enter (most) auto constants into Constants.h

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -8,6 +8,8 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+// VERY important for auto constants
+#include "units/units.h"
 /**
  * The Constants header provides a convenient place for teams to hold robot-wide
  * numerical or boolean constants.  This should not be used for any other
@@ -21,7 +23,20 @@
 
 namespace DriveConstants
 {
-
+    // AUTO CONSTANTS
+    // placeholder values until measured with FRC Characterization Tool - voltage
+    constexpr auto ks = 0.22_V;
+    constexpr auto kv = 1.98 * 1_V * 1_s / 1_m;
+    constexpr auto ka = 0.2 * 1_V * 1_s * 1_s / 1_m;
+    constexpr double kPDriveVel = 8.5;
+    // distance between wheels for differential drive
+    constexpr auto kTrackwidth = 0.609_m;
+    // max speed/accelerations
+    constexpr auto kMaxSpeed = 4_mps;
+    constexpr auto kMaxAcceleration = 3_mps_sq;
+    // RAMSETE follower values
+    constexpr double kRamseteB = 2;
+    constexpr double kRamseteZeta = 0.7;
 }
 
 #endif //CONSTANTS_H
