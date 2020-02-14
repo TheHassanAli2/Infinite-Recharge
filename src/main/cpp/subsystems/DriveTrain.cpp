042159a1af363efe57656ce2da2a8de2a989e169
Daniel Agapov <dlern1@ocdsb.ca>
2020-02-14 17:38:11 -0500

    Started Auto incoprporation into DriveTrain

--- src/main/cpp/subsystems/DriveTrain.cpp
+++ src/main/cpp/subsystems/DriveTrain.cpp
@@ -8,6 +8,10 @@
 #include "subsystems/DriveTrain.h"
 #include <frc/VictorSP.h>
 
+#include <frc/geometry/Rotation2d.h>
+#include <frc/kinematics/DifferentialDriveWheelSpeeds.h>
+
+
 using namespace DriveConstants;
 
 namespace frc4783 {
@@ -22,6 +26,18 @@ DriveTrain::DriveTrain()
     leftDrive.reset(new frc::SpeedControllerGroup(*backLeftMotor, *frontLeftMotor));
     rightDrive.reset(new frc::SpeedControllerGroup(*backRightMotor, *frontRightMotor));
     differentialDrive.reset(new frc::DifferentialDrive(*leftDrive, *rightDrive));
+    m_odometry.reset(new frc::DifferentialDriveOdometry());
+    m_leftEncoder.reset(new frc::Encoder());
+    m_rightEncoder.reset(new frc::Encoder());
+    m_leftEncoder{kLeftEncoderPorts[0], kLeftEncoderPorts[1]},
+    m_rightEncoder{kRightEncoderPorts[0], kRightEncoderPorts[1]},
+    m_odometry{frc::Rotation2d(units::degree_t(GetHeading()))} 
+    // Set the distance per pulse for the encoders
+    m_leftEncoder.SetDistancePerPulse(kEncoderDistancePerPulse);
+    m_rightEncoder.SetDistancePerPulse(kEncoderDistancePerPulse);
+
+    ResetEncoders();
+
 
 
     frontLeftMotor->SetInverted(true);
@@ -35,6 +51,9 @@ DriveTrain::DriveTrain()
 
 void DriveTrain::Periodic()
 {
+    m_odometry.Update(frc::Rotation2d(units::degree_t(GetHeading())),
+    units::meter_t(m_leftEncoder.GetDistance()),
+    units::meter_t(m_rightEncoder.GetDistance()));
 }
 
 void DriveTrain::ArcadeDrive(double speed, double turn)
@@ -52,6 +71,51 @@ void DriveTrain::Stop()
 void DriveTrain::TankDrive(double leftspeed, double rightspeed)
 {
     differentialDrive->TankDrive(leftspeed, rightspeed);
+}
+
+void DriveTrain::TankDriveVolts(units::volt_t left, units::volt_t right) {
+    leftDrive.SetVoltage(left);
+    rightDrive.SetVoltage(-right);
+    m_drive.Feed();
+}
+
+void DriveTrain::ResetEncoders() {
+    m_leftEncoder.reset();
+    m_rightEncoder.reset();
+}
+
+double DriveTrain::GetAverageEncoderDistance() {
+    return (m_leftEncoder.GetDistance() + m_rightEncoder.GetDistance()) / 2.0;
+}
+
+void DriveTrain::SetMaxOutput(double maxOutput) {
+    m_drive.SetMaxOutput(maxOutput);
+}
+
+frc::Encoder DriveTrain::GetLeftEncoder() { return m_leftEncoder; }
+
+frc::Encoder DriveTrain::GetRightEncoder() { return m_rightEncoder; }
+
+
+frc::DifferentialDriveWheelSpeeds DriveTrain::GetWheelSpeeds() {
+    return {units::meters_per_second_t(m_leftEncoder.GetRate()),
+    units::meters_per_second_t(m_rightEncoder.GetRate())};
+}
+
+double DriveTrain::GetHeading() {
+    return std::remainder(m_gyro.GetAngle(), 360) * (kGyroReversed ? -1.0 : 1.0);
+}
+
+double DriveTrain::GetTurnRate() {
+    return m_gyro.GetRate() * (kGyroReversed ? -1.0 : 1.0);
+}
+
+frc::Pose2d DriveTrain::GetPose() { return m_odometry.GetPose(); }
+
+void DriveTrain::ResetOdometry(frc::Pose2d pose) {
+    ResetEncoders();
+    m_odometry.ResetPosition(pose,
+    frc::Rotation2d(units::degree_t(GetHeading())));
 }
 
 } /* namespace frc4783 */
--- src/main/include/subsystems/DriveTrain.h
+++ src/main/include/subsystems/DriveTrain.h
@@ -12,6 +12,13 @@
 #include <frc/SpeedControllerGroup.h>
 #include <frc/drive/DifferentialDrive.h>
 #include <Constants.h>
+
+#include <frc/AnalogGyro.h>
+#include <frc/Encoder.h>
+#include <frc/PWMVictorSPX.h>
+#include <frc/geometry/Pose2d.h>
+#include <frc/kinematics/DifferentialDriveOdometry.h>
+#include <units/units.h>
 //#include <Robot.h>
 //#include <commands/DriveWithJoystick.h>
 #include "ctre/Phoenix.h"
@@ -32,6 +39,85 @@ public:
 
     // remember to put something here later ya feel?
 
+    /**
+   * Controls each side of the drive directly with a voltage.
+   *
+   * @param left the commanded left output
+   * @param right the commanded right output
+   */
+  void TankDriveVolts(units::volt_t left, units::volt_t right);
+
+
+  /**
+   * Resets the drive encoders to currently read a position of 0.
+   */
+  void ResetEncoders();
+
+  /**
+   * Gets the average distance of the TWO encoders.
+   *
+   * @return the average of the TWO encoder readings
+   */
+  double GetAverageEncoderDistance();
+
+  /**
+   * Gets the left drive encoder.
+   *
+   * @return the left drive encoder
+   */
+  frc::Encoder GetLeftEncoder();
+
+  /**
+   * Gets the right drive encoder.
+   *
+   * @return the right drive encoder
+   */
+  frc::Encoder GetRightEncoder();
+
+  /**
+   * Sets the max output of the drive.  Useful for scaling the drive to drive
+   * more slowly.
+   *
+   * @param maxOutput the maximum output to which the drive will be constrained
+   */
+  void SetMaxOutput(double maxOutput);
+
+  /**
+   * Returns the heading of the robot.
+   *
+   * @return the robot's heading in degrees, from 180 to 180
+   */
+  double GetHeading();
+
+  /**
+   * Returns the turn rate of the robot.
+   *
+   * @return The turn rate of the robot, in degrees per second
+   */
+  double GetTurnRate();
+
+  /**
+   * Returns the currently-estimated pose of the robot.
+   *
+   * @return The pose.
+   */
+  frc::Pose2d GetPose();
+
+  /**
+   * Returns the current wheel speeds of the robot.
+   *
+   * @return The current wheel speeds.
+   */
+  frc::DifferentialDriveWheelSpeeds GetWheelSpeeds();
+
+  /**
+   * Resets the odometry to the specified pose.
+   *
+   * @param pose The pose to which to set the odometry.
+   */
+  void ResetOdometry(frc::Pose2d pose);
+
+
 private:
     std::shared_ptr<frc::SpeedController> frontLeftMotor;
     std::shared_ptr<frc::SpeedController> backLeftMotor;
@@ -40,6 +126,10 @@ private:
     std::shared_ptr<frc::DifferentialDrive> differentialDrive;
     std::shared_ptr<frc::SpeedControllerGroup> leftDrive;
     std::shared_ptr<frc::SpeedControllerGroup> rightDrive;
+    std::shared_ptr<frc::Encoder> m_leftEncoder;
+    std::shared_ptr<frc::Encoder> m_rightEncoder;
+    std::shared_ptr<frc::AnalogGyro> m_gyro;
+    std::shared_ptr<frc::DifferentialDriveOdometry> m_odometry;
 };
 
 } /* namespace frc4783 */
